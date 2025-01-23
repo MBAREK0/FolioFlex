a4c15e1cd8845f2c2452ca8702a7be9da91b78f0
MBAREK0 <elaadraouimbarek2023@gmail.com>
2025-01-23 11:06:44 +0100

    first commit

---
+++ src/main/java/org/mbarek0/folioflex/FolioFlexApplication.java
@@ -0,0 +1,13 @@
+package org.mbarek0.folioflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolioFlexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FolioFlexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mbarek0/folioflex/FolioFlexApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mbarek0.folioflex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolioFlexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
